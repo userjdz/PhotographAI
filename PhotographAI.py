924ab4925990284a1e996742e1775a32ba60b8a0
J4DR3Z <70348839+J4DR3Z@users.noreply.github.com>
2023-02-15 21:17:06 -0500

    Update and rename fr2.py to PhotographAI.py

--- PhotographAI.py
+++ PhotographAI.py
@@ -19,13 +19,13 @@ for filename in os.listdir(folder_a):
 
     # Cargar la imagen
     filenamea.append(filename)
-    print(filename)
+    #print(filename)
     image = (cv2.imread(os.path.join(folder_a, filename)))
-    print("imagen leida")
+    #print("imagen leida")
     face_loc= (face_recognition.face_locations(image)[0])
-    print("rostro detectado")
+    #print("rostro detectado")
     face_image_encodings.append(face_recognition.face_encodings(image, known_face_locations=[face_loc], model="large")[0])
-    print("rostro codificado")
+    #print("rostro codificado")
     face_folder = os.path.join(detected_faces_folder, f"{filename}")
     if not os.path.exists(face_folder):
         os.makedirs(face_folder)
@@ -38,9 +38,9 @@ for filename in os.listdir(folder_b):
     # Cargar la imagen
     found=False
     imageC = cv2.imread(os.path.join(folder_b, filename))
-    print("imagen: ", filename,"  leida")
+    #print("imagen: ", filename,"  leida")
     face_locations = face_recognition.face_locations(imageC)
-    print("posiciones de rostros: ",face_locations)
+    #print("posiciones de rostros: ",face_locations)
     if face_locations != []:
           for face_location in face_locations:
                face_frame_encodings = face_recognition.face_encodings(imageC, known_face_locations=[face_location], model= "large")[0]
@@ -49,11 +49,11 @@ for filename in os.listdir(folder_b):
                     result = face_recognition.compare_faces([face_image_encoding], face_frame_encodings,0.5)
                     if result[0] == True:
                         found=True
-                        print("--------------------MATCH-----------------------------")
+                        #print("--------------------MATCH-----------------------------")
                         face_folder= os.path.join(os.path.join(ruta_directorio_archivo,"detected_faces"),filenamea[face_image_encoding_index])
-                        print(face_folder)
-                        print(face_image_encoding_index)
-                        print(filename)
+                        #print(face_folder)
+                        #print(face_image_encoding_index)
+                        #print(filename)
                         #reponer el face folder en la siguiente linea
                         cv2.imwrite(os.path.join(face_folder, filename), imageC)
                     face_image_encoding_index +=1
