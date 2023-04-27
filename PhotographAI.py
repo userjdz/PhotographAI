e82a6f170eafa59b42782d28f995a4db0aef11f1
J4DR3Z <70348839+J4DR3Z@users.noreply.github.com>
2023-04-27 11:50:12 -0500

    Update PhotographAI.py

--- PhotographAI.py
+++ PhotographAI.py
@@ -3,8 +3,8 @@ import face_recognition
 import os
 ruta_directorio_archivo = os.path.dirname(os.path.abspath(__file__))
 Fmodel="cnn"
-index=0
-face_image_encodings=[]
+Face_A_Codes=[]
+Face_A_Code_index=0
 face_loc =[]
 image=[]
 filenamea=[]
@@ -24,13 +24,12 @@ for filename in os.listdir(folder_a):
     #print("imagen leida")
     face_loc= (face_recognition.face_locations(image)[0])
     #print("rostro detectado")
-    face_image_encodings.append(face_recognition.face_encodings(image, known_face_locations=[face_loc], model="large")[0])
+    Face_A_Codes.append(face_recognition.face_encodings(image, known_face_locations=[face_loc], model="large")[0])
     #print("rostro codificado")
     face_folder = os.path.join(detected_faces_folder, f"{filename}")
     if not os.path.exists(face_folder):
         os.makedirs(face_folder)
     cv2.imwrite(os.path.join(face_folder, filename), image)
-    index +=1
     
 
 folder_b = os.path.join(ruta_directorio_archivo,"B")
@@ -38,25 +37,31 @@ for filename in os.listdir(folder_b):
     # Cargar la imagen
     found=False
     imageC = cv2.imread(os.path.join(folder_b, filename))
-    #print("imagen: ", filename,"  leida")
+    print("imagen: ", filename,"  leida")
     face_locations = face_recognition.face_locations(imageC)
-    #print("posiciones de rostros: ",face_locations)
+    print("posiciones de rostros: ",face_locations)
+    if face_locations == []:
+        nff_folder =os.path.join(os.path.join(ruta_directorio_archivo,"detected_faces"),"NOT_FACES_FOUND")
+        if not os.path.exists(nff_folder):
+            os.makedirs(nff_folder)
+        cv2.imwrite(os.path.join(nff_folder, filename), imageC)
     if face_locations != []:
           for face_location in face_locations:
-               face_frame_encodings = face_recognition.face_encodings(imageC, known_face_locations=[face_location], model= "large")[0]
-               face_image_encoding_index=0
-               for face_image_encoding in face_image_encodings:
-                    result = face_recognition.compare_faces([face_image_encoding], face_frame_encodings,0.5)
+               Face_B_Code = face_recognition.face_encodings(imageC, known_face_locations=[face_location], model= "large")[0]
+               Face_A_Code_index=0
+               for Face_A_Code in Face_A_Codes:
+                    result = face_recognition.compare_faces([Face_A_Code], Face_B_Code,0.5)
                     if result[0] == True:
                         found=True
-                        #print("--------------------MATCH-----------------------------")
-                        face_folder= os.path.join(os.path.join(ruta_directorio_archivo,"detected_faces"),filenamea[face_image_encoding_index])
-                        #print(face_folder)
-                        #print(face_image_encoding_index)
-                        #print(filename)
+                        print("--------------------MATCH-----------------------------")
+                        face_folder= os.path.join(os.path.join(ruta_directorio_archivo,"detected_faces"),filenamea[Face_A_Code_index])
+                        print(face_folder)
+                        print(Face_A_Code_index)
+                        print(filename)
                         #reponer el face folder en la siguiente linea
                         cv2.imwrite(os.path.join(face_folder, filename), imageC)
-                    face_image_encoding_index +=1
+                    Face_A_Code_index +=1
+        
     #En caso de no encontrar la persona en la imagen, va a NOT_FOUND
     if not found:
         nf_folder =os.path.join(os.path.join(ruta_directorio_archivo,"detected_faces"),"NOT_FOUND")
